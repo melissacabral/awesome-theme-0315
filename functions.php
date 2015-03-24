8cf58517e8ad8344765cee6dd574fb6369ccad45
melissacabral <melissacabral@gmail.com>
2015-03-23 21:25:06 -0700

    after day 3
    added front-page, functions

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+//use this file for custom functions and activating 'sleeping' wordpress features
+//allow you to attach a "featured image" to each post or page
+add_theme_support('post-thumbnails');
+
+add_image_size( 'big-banner', 1300, 300, true );
+
+
+
+//no close PHP!
